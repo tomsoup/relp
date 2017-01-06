03e816d4316b0b67fb12b5175c4913f3fa61a013
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-01-05 19:58:00 -0500

    Images with s3 and figaro

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV["S3_RES_ACCESS_KEY_ID"],
+      :aws_secret_access_key => ENV["S3_RES_SECRET_ACCESS_KEY_ID"]
+    }
+    config.fog_directory = ENV["RES_DIRECTORY"]
+  end
+end
